9b7081f0fb5677c5f117ca51bc78003c47871176
jPOS-GroupSelector <saqebnadeem1@gmail.com>
2022-06-23 17:17:42 +0530

    jPos

--- jPOS-prc/src/main/java/com/sn/listener/ServerApplicationListener.java
+++ jPOS-prc/src/main/java/com/sn/listener/ServerApplicationListener.java
@@ -9,6 +9,14 @@ import org.jpos.iso.ISOSource;
 import org.jpos.space.Space;
 import org.jpos.space.SpaceFactory;
 import org.jpos.transaction.Context;
+
+// we don't make use of server object here as
+// from JPOS user guide this approach is not advisable,so we should be using Q2
+// not connecting through channel by hardcoding but by using the .xml QBean ,
+// but the class Q2 is responsible for connecting
+// to the server as its QBean is in resources/deploy directory
+
+// this listener is called by listener.xml
 public class ServerApplicationListener implements ISORequestListener,Configurable{
     private Configuration configuration;
     @Override
@@ -17,6 +25,7 @@ public class ServerApplicationListener implements ISORequestListener,Configurabl
     }
     @Override
     public boolean process(ISOSource isoSource, ISOMsg isoMsg) {
+        System.out.println("SERVER IS ACTIVATED BY SAQEB");
         String spaceN = configuration.get("space");
         long timeout = configuration.getLong("spaceTimeout");
         String queueN = configuration.get("queue");
@@ -29,6 +38,7 @@ public class ServerApplicationListener implements ISORequestListener,Configurabl
         context.put(Constants.RESPONSE_KEY,respMsg);//so we can use it to modify in sender-response
         context.put(Constants.RESOURCE_KEY,isoSource);//without changing the original iso-msg
         space.out(queueN,context,timeout);//throwing into space is now accessible to the txnmgr.xml
+        //as soon as we put the ctx obj to space it is picked up by txnmgr where it selects the participants
         return true;
     }
 }
